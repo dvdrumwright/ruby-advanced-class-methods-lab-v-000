25121b7c7e067a7f38af8156ac2635938ee0390c
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-05-18 18:46:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,7 +62,7 @@ def self.new_from_filename(filename)
     
     
     def self.create_from_filename(file)
-      a = file.gsub(".mp3","")split(" - ")
+      a = file.gsub(".mp3","").split(" - ")
       song = self.create_from_name(a[0])
       song.song_name(a[1])
        
